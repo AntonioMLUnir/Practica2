16c80550ea195c2dba4baff1dca3caeaffb004f7
AntonioMLUnir <antonio.medina865@comunidadunir.net>
2026-03-18 22:44:05 +0100

    finalizada configuración Ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,3 +70,38 @@ resource "azurerm_network_interface" "nic" {
 		public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
+
+resource "azurerm_network_security_group" "nsg" {
+	name = "vm-nsg"
+	location = azurerm_resource_group.rg.location
+	resource_group_name = azurerm_resource_group.rg.name
+
+	security_rule {
+		name = "SSH"
+		priority = 1000
+		direction = "Inbound"
+		access = "Allow"
+		protocol = "Tcp"
+		source_port_range = "*"
+		destination_port_range = "22"
+		source_address_prefix = "*"
+		destination_address_prefix = "*"
+	}
+
+	security_rule {
+		name                       = "allow-8080"
+		priority                   = 300
+		direction                  = "Inbound"
+		access                     = "Allow"
+		protocol                   = "Tcp"
+		source_port_range          = "*"
+		destination_port_range     = "8080"
+		source_address_prefix      = "*"
+		destination_address_prefix = "*"
+	}
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+	network_interface_id = azurerm_network_interface.nic.id
+	network_security_group_id = azurerm_network_security_group.nsg.id
+}
